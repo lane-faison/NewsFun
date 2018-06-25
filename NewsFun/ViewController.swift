a09bc2797597d3a918391c871262b4772bf81362
Lane Faison <lane.faison@icloud.com>
2018-06-24 22:41:10 -0600

    Initial Commit

---
+++ NewsFun/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NewsFun
+//
+//  Created by Lane Faison on 6/24/18.
+//  Copyright © 2018 Lane Faison. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
