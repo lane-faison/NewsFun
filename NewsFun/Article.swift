6b1ffdd51d9b71f799503a7aef01ae8dc5be099b
Lane Faison <lane.faison@icloud.com>
2018-06-28 22:45:00 -0600

    Article and Articles made Decodable and parsing working

--- NewsFun/Article.swift
+++ NewsFun/Article.swift
@@ -3,6 +3,7 @@ import Foundation
 public struct Articles: Decodable {
     var articles: [Article]
     var totalResults: Int
+    var status: String
     
     public init?(json: JSON) {
         do {
@@ -15,19 +16,51 @@ public struct Articles: Decodable {
 }
 
 public struct Article: Decodable {
-    var title: String?
+    var source: Source
     var author: String?
-    var description: String
-    var urlToImage: URL?
+    var title: String?
+    var description: String?
     var url: URL?
-    var category: String?
+    var urlToImage: URL?
+    var publishedAt: Date?
     
-    public init?(json: JSON) {
-        do {
-            self = try JSONDecoder().decode(Article.self, from: json.JSON())
-        } catch {
-            assertionFailure("There was an error parsing Articles")
-            return nil
+    enum CodingKeys: String, CodingKey {
+        case source
+        case author
+        case title
+        case description
+        case urlString = "url"
+        case urlToImageString = "urlToImage"
+        case publishedAtString = "publishedAt"
+    }
+    
+    public struct Source: Decodable {
+        var id: String?
+        var name: String
+        
+        enum CodingKeys: String, CodingKey {
+            case id
+            case name
+        }
+    }
+    
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        source = try container.decode(Source.self, forKey: .source)
+        author = try container.decodeIfPresent(String.self, forKey: .author)
+        title = try container.decodeIfPresent(String.self, forKey: .title)
+        description = try container.decodeIfPresent(String.self, forKey: .description)
+        
+        if let urlString = try container.decodeIfPresent(String.self, forKey: .urlString) {
+            url = URL(string: urlString)
+        }
+        if let urlToImageString = try container.decodeIfPresent(String.self, forKey: .urlToImageString) {
+            urlToImage = URL(string: urlToImageString)
+        }
+        if let publishedAtString = try container.decodeIfPresent(String.self, forKey: .publishedAtString) {
+            let dateFormatter = DateFormatter()
+            publishedAt = dateFormatter.date(from: publishedAtString)
         }
     }
 }
--- NewsFun/NewsHelper.swift
+++ NewsFun/NewsHelper.swift
@@ -5,8 +5,9 @@ class NewsHelper {
     
     func getArticles() {
         Alamofire.request("https://newsapi.org/v2/top-headlines?country=us&apiKey=dce0ac82107c442b9d83f38350d2fcc9").responseJSON { (response) in
-            if let articles = response.result.value as? Articles {
-                print(articles.totalResults)
+            if let articlesJSON = response.result.value as? JSON {
+                guard let articles = Articles(json: articlesJSON) else { return }
+                //
             }
         }
     }
